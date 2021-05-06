0bcc2e52a43454b9892bb14e375c5b95102f83a6
thiago miziara <thiagonmiziara@gmail.com>
2021-05-06 18:51:20 -0300

    add responsivo no componente table

--- src/pages/Users/index.tsx
+++ src/pages/Users/index.tsx
@@ -1,11 +1,33 @@
-import { Box, Button, Flex,Icon, Heading, Table,Thead,Tr,Th,Td, Checkbox, Tbody, Text } from "@chakra-ui/react";
 import React from "react";
+import {
+  Box,
+  Button,
+  Flex,
+  Icon,
+  Heading,
+  Table,
+  Thead,
+  Tr,
+  Th,
+  Td,
+  Checkbox,
+  Tbody,
+  Text,
+  useBreakpointValue,
+} from "@chakra-ui/react";
+
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
+
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
+
   return (
     <Box>
       <Header />
@@ -23,53 +45,57 @@ export default function UserList() {
               size="sm"
               fontSize="sm"
               colorScheme="pink"
-              leftIcon={<Icon
-              as={RiAddLine} fontSize="20"/>}
+              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
             >
               Criar novo
             </Button>
           </Flex>
-        
+
           <Table colorScheme="whiteAlpha">
             <Thead>
               <Tr>
-                <Th px="6" color="gray.300" width="8">
-                  <Checkbox colorScheme="pink"/>
+                <Th px={["4", "4", "6"]} color="gray.300" width="8">
+                  <Checkbox colorScheme="pink" />
                 </Th>
                 <Th>Usuário</Th>
-                <Th>Data de cadastro</Th>
+                {isWideVersion && <Th>Data de cadastro</Th>}
                 <Th w="8"></Th>
               </Tr>
             </Thead>
             <Tbody>
               <Tr>
-                <Td px="6">
-                  <Checkbox colorScheme="pink"/>
+                <Td px={["4", "4", "6"]}>
+                  <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
                   <Box>
                     <Text fontWeight="bold">Thiago Nunes</Text>
-                    <Text fontSize="sm" color="gray.300">thiagonmiziara@gmail.com</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      thiagonmiziara@gmail.com
+                    </Text>
                   </Box>
                 </Td>
-                <Td>04 de Abril,2021</Td>
-                <Td>
-                  <Button
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="purple"
-                    leftIcon={<Icon
-                    as={RiPencilLine} fontSize="16"/>}
-                  >
-                    Editar
-                  </Button>
-                </Td>
+                {isWideVersion && <Td>04 de Abril,2021</Td>}
+                {isWideVersion ? (
+                  <Td>
+                    <Button
+                      as="a"
+                      size="sm"
+                      fontSize="sm"
+                      colorScheme="pink"
+                      leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                    >
+                      Editar
+                    </Button>
+                  </Td>
+                ) : (
+                  ""
+                )}
               </Tr>
             </Tbody>
-          </Table>   
+          </Table>
 
-          <Pagination/>     
+          <Pagination />
         </Box>
       </Flex>
     </Box>
