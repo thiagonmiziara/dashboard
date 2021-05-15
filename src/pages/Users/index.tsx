c2bbc1746ec9b60050d91d705d3a5851e6a02a8f
thiago miziara <thiagonmiziara@gmail.com>
2021-05-15 17:21:24 -0300

    add state do react-query

--- src/pages/Users/index.tsx
+++ src/pages/Users/index.tsx
@@ -30,8 +30,24 @@ export default function UserList() {
     const response = await fetch("http://localhost:3000/api/users");
     const data = await response.json();
 
-    return data;
+    const users = data.users.map(user =>{
+      return{
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric'
+        })
+      };
   });
+
+    return users;
+  },{
+    staleTime: 1000 * 5,// 5 seconds
+  }
+  );
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -85,36 +101,27 @@ export default function UserList() {
                     <Th w="8"></Th>
                   </Tr>
                 </Thead>
+
                 <Tbody>
-                  <Tr>
-                    <Td px={["4", "4", "6"]}>
-                      <Checkbox colorScheme="pink" />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight="bold">Thiago Nunes</Text>
-                        <Text fontSize="sm" color="gray.300">
-                          thiagonmiziara@gmail.com
-                        </Text>
-                      </Box>
-                    </Td>
-                    {isWideVersion && <Td>04 de Abril,2021</Td>}
-                    {/* {isWideVersion ? (
-                  <Td>
-                    <Button
-                      as="a"
-                      size="sm"
-                      fontSize="sm"
-                      colorScheme="pink"
-                      leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                    >
-                      Editar
-                    </Button>
-                  </Td>
-                ) : (
-                  ""
-                )} */}
-                  </Tr>
+                  {data.map(user =>{
+                    return(
+                      <Tr key={user.id}>
+                      <Td px={["4", "4", "6"]}>
+                        <Checkbox colorScheme="pink" />
+                      </Td>
+                      <Td>
+                        <Box>
+                          <Text fontWeight="bold">{user.name}</Text>
+                          <Text fontSize="sm" color="gray.300">
+                            {user.email}
+                          </Text>
+                        </Box>
+                      </Td>
+                      {isWideVersion && <Td>{user.createdAt}</Td>}
+               
+                    </Tr>
+                    );
+                  })}
                 </Tbody>
               </Table>
 
