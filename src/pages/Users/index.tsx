52245960def8be5b5cd9e5f7b4b4d201ef1f4c9a
thiago miziara <thiagonmiziara@gmail.com>
2021-05-16 18:37:59 -0300

    add axios e configurndo baseUrl

--- src/pages/Users/index.tsx
+++ src/pages/Users/index.tsx
@@ -1,4 +1,3 @@
-import React, { useEffect } from "react";
 import Link from "next/link";
 import {
   Box,
@@ -18,17 +17,18 @@ import {
   Spinner,
 } from "@chakra-ui/react";
 
-import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { RiAddLine } from "react-icons/ri";
 import { useQuery } from "react-query";
 
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
+import { api } from "../../services/api";
 
 export default function UserList() {
   const { data, isLoading,isFetching, error } = useQuery("users", async () => {
-    const response = await fetch("http://localhost:3000/api/users");
-    const data = await response.json();
+    const {data} = await api.get("/users");
+    
 
     const users = data.users.map(user =>{
       return{
